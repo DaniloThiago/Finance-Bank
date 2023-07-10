4be043e5b47fe062f7577d764dcbcbc1c4e2c38c
Ana Carolina <anacarolinaneta@hotmail.com>
2023-07-10 07:52:37 -0300

    alterações na tela dos cartões

--- src/pages/Wallet.svelte
+++ src/pages/Wallet.svelte
@@ -1,5 +1,66 @@
-<h1>Wallet</h1>
-<spline-viewer url="https://prod.spline.design/FVZWbQH2B6ndj9UU/scene.splinecode" events-target="local"></spline-viewer>
-<style>
-  h1 { color: aliceblue; }
+<script lang="ts">
+  import NewCard from "../components/NewCard.svelte";
+import SelectCard from "../components/SelectCard.svelte";
+
+
+</script>
+  
+<div class="d-flex flex-col title-div">
+  <p class="title">Meus Cartões</p>
+  <p class="subtitle">Gerencie seus cartões, podendo excluir ou adicionar novos</p>
+</div>
+
+<div class="content d-flex flex-col align-center">
+  <div class="d-flex justify-between w-100">
+    <p class="title">Cartões Físicos</p>
+    <NewCard />
+  </div>
+  <SelectCard />
+</div>
+<div class="content d-flex flex-col align-center">
+  <div class="d-flex justify-between w-100">
+    <p class="title">Cartões Virtuais</p>
+    <NewCard is_virtual={true} />
+  </div>
+  <SelectCard is_virtual={true} />
+</div>
+
+<style lang="scss">
+  .title-div{
+    padding-bottom: 32px;
+    gap: 8px;
+
+    .title{
+      color: #FFF;
+      font-size: 2.4rem;
+      font-weight: var(--s-bold);
+      line-height: 120%;
+    }
+
+    .subtitle{
+      color: #A6A6A6;
+      font-size: 1.6rem;  
+      line-height: 160%;
+    }
+  }
+  
+  .content{
+      padding: 2.8rem 2.4rem;
+      gap: 4rem;
+      border-radius: 2rem;
+      background: var(--c-ebony-clay);
+      gap: 4rem;
+      margin-bottom: 5.4rem;
+      width: calc(100vw - (33rem + var(--gap) * 3));
+
+      div{
+
+        .title{
+          color: var(--c-white);
+          font-size: 2rem;
+          font-weight: var(--s-bold);
+          line-height: 120%;
+        }
+      }
+  }
 </style>
