bdf46cb09eeda9949b586832db79e07fa408bdd8
Ana Carolina <anacarolinaneta@hotmail.com>
2023-07-10 07:43:33 -0300

    criacao de componente modal

---
+++ src/components/Modal.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+    export let modal_toggle : boolean = false;
+
+</script>
+
+{#if modal_toggle}
+<div class="modal d-flex flex-col align-center justify-center w-100">
+    <div class="modal--content d-flex flex-col align-center"> 
+        <slot></slot>
+    </div>
+</div>
+{/if}
+
+<style lang="scss">
+    .modal{
+        position: absolute;
+        z-index: 1000;
+        left: 0;
+        top: 0;
+        height: 100vh;
+        background-color: #1D1D4166;
+
+        .modal--content{
+            width: 414px;
+            padding: 24px 32px 40px 32px;
+            gap: 40px;
+            border-radius: 1rem;
+            background: var(--c-haiti);
+            box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        }   
+    }
+</style>
