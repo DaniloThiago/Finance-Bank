b9e9b47b341b14bf3c343bd272abaac9ea5e950d
Ana Carolina <anacarolinaneta@hotmail.com>
2023-07-10 07:38:48 -0300

    alterações menu

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -8,11 +8,13 @@ interface MenuItem {
 }
 
 import Dashboard from '../pages/Dashboard.svelte';
+import TransactionsPage from '../pages/TransactionsPage.svelte';
 import Analytics from '../pages/Analytics.svelte';
 import Wallet from '../pages/Wallet.svelte';
 
 export const menuItems: MenuItem[] = [
   { description: 'Dashboard', icon: 'dashboard', active: true, component: Dashboard },
-  { description: 'Analytics', icon: 'analytics', active: false, component: Analytics },
-  { description: 'My Wallet', icon: 'wallet', active: false, component: Wallet },
+  { description: 'Transações', icon: 'attach_money', active: false, component: TransactionsPage },
+  { description: 'Análise dos Gastos', icon: 'bar_chart', active: false, component: Analytics },
+  { description: 'Meus Cartões', icon: 'style', active: false, component: Wallet },
 ];
